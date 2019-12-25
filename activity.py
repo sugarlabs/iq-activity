16adf96d3bd0fdb3106b9cfa899954c8848159fe
srevinsaju <srevin03@gmail.com>
2019-12-25 23:00:09 +0300

    Port to Python3, Update sugargame, transfer delocalized code to one file

--- activity.py
+++ activity.py
@@ -13,14 +13,37 @@ from sugar3.graphics.toolbarbox import ToolbarBox
 from sugar3.activity.widgets import ActivityToolbarButton
 from sugar3.activity.widgets import StopButton
 from sugar3.graphics.toolbarbox import ToolbarButton
+from sugar3.activity.activity import PREVIEW_SIZE
 import sugargame.canvas
 import load_save
+
 import IQ
 
 class PeterActivity(activity.Activity):
     def __init__(self, handle):
-        super(PeterActivity, self).__init__(handle)
+        activity.Activity.__init__(self, handle)
 
+        self.max_participants = 1
+
+        # Create the game instance.
+        self.game = IQ.IQ(self)
+
+        # Call the toolbar method
+        
+
+        # Note that set_canvas implicitly calls
+        # read_file when resuming from the Journal.
+        self._pygamecanvas = sugargame.canvas.PygameCanvas(
+                self, main=self.game.run,
+                modules=[pygame.display])
+
+        self.set_canvas(self._pygamecanvas)
+        self._pygamecanvas.grab_focus()
+        self.build_toolbar()
+        # Start the game running.
+        # self._pygamecanvas.run_pygame(self.game.run)
+
+    def build_toolbar(self):
         # Build the activity toolbar.
         toolbar_box = ToolbarBox()
         self.set_toolbar_box(toolbar_box)
@@ -35,36 +58,24 @@ class PeterActivity(activity.Activity):
         separator.set_expand(True)
         toolbar_box.toolbar.insert(separator, -1)
         separator.show()
-
-
         stop_button = StopButton(self)
         toolbar_box.toolbar.insert(stop_button, -1)
         stop_button.show()
         self.show_all()
 
-        # Create the game instance.
-        self.game = IQ.IQ()
-
-        # Build the Pygame canvas.
-        self._pygamecanvas = \
-            sugargame.canvas.PygameCanvas(self)
-        # Note that set_canvas implicitly calls
-        # read_file when resuming from the Journal.
-        self.set_canvas(self._pygamecanvas)
-        self.game.canvas=self._pygamecanvas
-
-        # Start the game running.
-        self._pygamecanvas.run_pygame(self.game.run)
 
     def read_file(self, file_path):
         try:
             f = open(file_path, 'r')
         except:
-            return #****
+            return  # ****
         load_save.load(f)
         f.close()
 
     def write_file(self, file_path):
         f = open(file_path, 'w')
-        load_save.save(f)
+        self.game.save(f)
         f.close()
+
+    def get_preview(self):
+        return self._pygamecanvas.get_preview()
